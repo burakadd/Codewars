0e2a92b6020698616cb862d193474333c3396bc6
burakadd <50999098+burakadd@users.noreply.github.com>
2019-08-27 21:43:24 +0300

    Task was successfully solved

---
+++ Create Phone Number.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83
+def create_phone_number(n):
+    string_number = "".join(map(str, n))
+    return '({}{}{}) {}{}{}-{}{}{}'.format(*string_number)
