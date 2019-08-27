02ae24d27127dfb2b2210719cc783aa9798069fe
Guck111 <7080446@gmail.com>
2019-08-27 21:22:11 +0300

    added link to this kata

--- Are they the 'same'.py
+++ Are they the 'same'.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com/kata/550498447451fbbd7600041c
 def comp(array1, array2):
     return sorted(array2) == list(map(lambda i: pow(i, 2), sorted(array1))) if all(
-    array is not None for array in (array1, array2)) else False
+        array is not None for array in (array1, array2)) else False
